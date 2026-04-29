28de8dd062d7144dcd520909cf191cb173bceabe
Ritam Mukherjee <mukherjeeritam60@gmail.com>
2026-04-30 00:36:49 +0530

    Remove duplicate PORT variable

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 const https = require('https'); // Required to bypass SSL errors
 
 const app = express();
-const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
